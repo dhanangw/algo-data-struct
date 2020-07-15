af0d9fe1b3db109057da1916ec091fb41dd47add
dhanangw <wibisono.dhanang4@gmail.com>
2020-07-15 21:55:49 +0700

    replace insertAtTail()

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -10,18 +10,6 @@ class LinkedList {
 
     private static Node head;
 
-    public static void insertToLinkedListAtTail(Node newNode) {
-        if (head == null) {
-            head = newNode;
-        } else if (head != null) {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
     public static void insertToLinkedListAtPosition(int value, int position) {
         Node newNode = new Node();
         newNode.value = value;
@@ -33,18 +21,17 @@ class LinkedList {
             return;
         } else {
             Node current = head;
-            try {
-                for (int i = 0; i < position - 2; i++) {
-                    // traverse to node position - 1
-                    current = current.next;
+            // traverse to node position - 1
+            for (int i = 0; i < position - 2; i++) {
+                if (current.next == null) {
+                    // if arrives at last node
+                    current.next = newNode;
+                    return;
                 }
-                newNode.next = current.next;
-                current.next = newNode;
-            } catch (Exception e) {
-                // if position is beyond LinkedList's length + 1
-                insertToLinkedListAtTail(newNode);
-                return;
+                current = current.next;
             }
+            newNode.next = current.next;
+            current.next = newNode;
         }
     }
 
@@ -110,6 +97,7 @@ class LinkedList {
         insertToLinkedListAtPosition(2, 3);
         insertToLinkedListAtPosition(3, 2);
         insertToLinkedListAtPosition(5, 1);
+        insertToLinkedListAtPosition(1, 9);
         deleteNodeInPosition(4);
         deleteNodeInPosition(9);
         reverseLinkedListRecursion(head);
