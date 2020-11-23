f45599b3dc5a7e4268d588207ecde353581e67d0
dhanangw <wibisono.dhanang4@gmail.com>
2020-11-23 20:30:59 +0700

    add BFS for GraphAdjacencyMatrix

--- graph/GraphAdjacencyMatrix.java
+++ graph/GraphAdjacencyMatrix.java
@@ -1,4 +1,7 @@
 // Undirected graph represented in AdjacencyMatrix
+import java.util.*;
+
+
 class GraphAdjacencyMatrix{
     boolean[][] adjacencyMatrix;
     int numOfNodes;
@@ -35,11 +38,49 @@ class GraphAdjacencyMatrix{
             this.adjacencyMatrix[destinationNode][sourceNode] = false;
         }
     }
+
+    public LinkedList<Integer> getConnectedNodes(int startNode){
+        LinkedList<Integer> connectedNodes = new LinkedList<Integer>();
+        boolean[] matrixRow = this.adjacencyMatrix[startNode];
+        for (int i = 0; i < matrixRow.length; i++){
+            if (matrixRow[i]){
+                connectedNodes.add(i);
+            }
+        }
+        return connectedNodes;
+    }
+
+    public void breadthFirstSearch(int startNode){
+        boolean[] nodesVisited = new boolean[this.numOfNodes];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        
+        // put startNode in nodesVisited,
+        // and get connected nodes of startNode 
+        // and put them in queue.
+        nodesVisited[startNode] = true;
+        queue.add(startNode);
+
+        while (queue.size() != 0){
+            int currentNode = queue.poll();
+            System.out.print(currentNode + ", ");
+            LinkedList<Integer> connectedNodes = getConnectedNodes(currentNode);
+            
+            for (int i = 0; i < connectedNodes.size(); i++){
+                int node = connectedNodes.get(i);
+                if (!nodesVisited[node]){
+                    nodesVisited[i] = true;
+                    queue.add(node);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args){
         GraphAdjacencyMatrix graph = new GraphAdjacencyMatrix(3, true);
         graph.addEdge(1, 2);
-        graph.addEdge(0, 2);
-        graph.removeEdge(0,2);
-        graph.printNodes();
+        graph.addEdge(0, 1);
+        // graph.removeEdge(0,2);
+        // graph.printNodes();
+        graph.breadthFirstSearch(0);
     }    
 }
