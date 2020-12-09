c62724a8c499035e9efd73c6b72fb1ae52387ffa
dhanangw <wibisono.dhanang4@gmail.com>
2020-12-09 22:24:42 +0700

    add 1528

---
+++ 1528.java
@@ -0,0 +1,53 @@
+
+/**
+ * Shuffle String
+ * 
+ * Example 1:
+ * Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+ * Output: "leetcode"
+ * Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+ * 
+ * Example 2:
+ * Input: s = "abc", indices = [0,1,2]
+ * Output: "abc"
+ * Explanation: After shuffling, each character remains in its position.
+ *
+ * Example 3:
+ * Input: s = "aiohn", indices = [3,1,4,2,0]
+ * Output: "nihao"
+ * 
+ * Example 4:
+ * Input: s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
+ * Output: "arigatou"
+ * 
+ * Example 5:
+ * Input: s = "art", indices = [1,0,2]
+ * Output: "rat"
+*/
+
+
+import java.util.Hashtable;
+import java.util.Arrays;
+
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        Hashtable<Integer, Character> hashTable = new Hashtable<Integer, Character>();
+        StringBuilder output = new StringBuilder();
+
+        // put each char in string to a hash table, along with it's index.
+        for (int i = 0; i < indices.length; i++){
+            hashTable.put(indices[i], s.charAt(i));
+        }
+
+        // sort indices
+        Arrays.sort(indices);
+        
+        // traverse through sorted array, put chars of indices to output string.
+        for (int i = 0; i < indices.length; i++){
+            output.append(hashTable.get(i));
+        }
+        
+        return output.toString();
+    }
+}
