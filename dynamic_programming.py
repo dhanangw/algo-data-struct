9eaafbb4ee1c592747b61f759114f612e73ae7e6
dhanangw <wibisono.dhanang4@gmail.com>
2022-05-03 19:56:29 +0700

    add grid_traveler* in dynamic programming

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -50,7 +50,55 @@ def fibonacci_memo(n: int, memo: Dict[int, int] = {}) -> int:
 
     return memo[n]
 
+def grid_traveler(m: int, n: int) -> int:
+    """Given m*n grid, return number of possible way to traverse the grid from top-left cell to bottom-right cell.
+    You can only move downward or rightward.
+
+    Uses normal recursion.
+
+    Time: O(2^(m+n)) because we need to calculate all possible combination of row-column.
+    Space: O(m+n) because maximum recursion depth is m+n
+
+    Example:
+        grid_traveler(2,3) returns 3.
+        grid_traveler(1,1) returns 0.
+        grid_traveler(3,3) returns 6.
+    """
+    if m <= 0 or n <= 0:
+        return 0
+    elif m == 1 and n == 1:
+        return 1
+
+    move_down_result = grid_traveler(m - 1, n)
+    move_right_result = grid_traveler(m, n - 1)
+
+    return move_down_result + move_right_result
+
+def grid_traveler_memo(m: int, n: int, memo: Dict[str, int] = {}) -> int:
+    """Given m*n grid, return number of possible way to traverse the grid from top-left cell to bottom-right cell.
+    You can only move downward or rightward.
+
+    Uses dynamic programming memoization.
+
+    Time: O(m*n) still not sure why.
+    Space: O(m+n) maximum recursion depth is is m+n
+    """
+    key = f"{m},{n}"
+
+    if key in memo:
+        return memo[key]
+    elif m <= 0 or n <= 0:
+        return 0
+    elif m == 1 and n == 1:
+        return 1
+
+    move_down_result = grid_traveler_memo(m - 1, n, memo)
+    move_right_result = grid_traveler_memo(m, n - 1, memo)
+    memo[key] = move_down_result + move_right_result
+
+    return memo[key]
 
 if __name__ == '__main__':
-    print(fibonacci_memo(5))
+    # print(fibonacci_memo(5))
+    print(grid_traveler_memo(18,18))
 
